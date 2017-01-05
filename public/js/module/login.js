42bb995a6e1e8f122aa704dc90dde8f3328f6830
unknown <chenlongke@xf9h.net>
2017-01-05 11:22:49 +0800

    登录界面美化

--- public/js/module/login.js
+++ public/js/module/login.js
@@ -144,4 +144,13 @@
 			});
 		return is_true;		
 	});
+	$.backstretch([
+        "/img/bg/1.jpg",
+        "/img/bg/2.jpg",
+        "/img/bg/3.jpg",
+        "/img/bg/4.jpg"
+        ], {
+          fade: 1000,
+          duration: 8000
+    });
 })(jQuery);
