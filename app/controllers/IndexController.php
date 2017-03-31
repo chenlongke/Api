c296d46f11dfe8052e646730dcbdef6adaa4ab82
clk528 <clk528@qq.com>
2017-03-31 17:54:01 +0800

    add files

--- app/controllers/IndexController.php
+++ app/controllers/IndexController.php
@@ -1,6 +1,6 @@
 <?php
 
-class IndexController extends BaseController {
+class IndexController extends Phalcon\Mvc\Controller {
 
     public function indexAction() {
 
@@ -12,13 +12,31 @@ class IndexController extends BaseController {
 
     	//this->log_info($rsa->encryptRSA("陈龙科",$this->config->RSA_PUBLIC_KEY));
         //$this->log_info($this->redis->get('clk'));
+
     	$this->view->region = json_decode($this->getLocationAction(),true);
-    	$this->view->title = "登录";    	
+    	$this->view->title = "登录";
     	$this->view->index = $this->getip();
-    	//$this->view->render('index','login')->finish();
         $this->view->pick("index/login");
     }
 
+    private function getip(){
+        if (getenv("HTTP_CLIENT_IP")){
+            $ip = getenv("HTTP_CLIENT_IP");
+        }else if(getenv("HTTP_X_FORWARDED_FOR")){
+            $ip = getenv("HTTP_X_FORWARDED_FOR");
+        }else if(getenv("REMOTE_ADDR")){
+            $ip = getenv("REMOTE_ADDR");
+        }else {
+            $ip = "Unknow";
+        }
+        return $ip == "127.0.0.1" ? "27.115.94.242" : $ip;
+    }
+
+    private function getLocationAction(){
+        $ip = $this->getip();
+        $ip == "127.0.0.1" ? $ip = "27.115.94.242" : '';        
+        return NetworkUtils::http("http://int.dpool.sina.com.cn/iplookup/iplookup.php?format=json&ip={$ip}",null);
+    }
     public function NotFoundAction(){
     	die("找不到页面");
     }
