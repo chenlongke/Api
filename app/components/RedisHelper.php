d57bc291af3054743b6974348d9d0a974f14f4f4
clk528 <clk528@qq.com>
2017-03-31 16:40:59 +0800

    add file's

--- app/components/RedisHelper.php
+++ app/components/RedisHelper.php
@@ -1,15 +1,9 @@
 <?php
-/**
- * Created by PhpStorm.
- * User: seaman
- * Date: 6/15/14
- * Time: 6:51 PM
- */
 
 class RedisHelper {
 
     public static function getredis4parm($ip,$port){
-        $redis = new Redis();
+        $redis = new \Redis();
         $redis->pconnect( $ip , $port );
         return $redis;
     }
