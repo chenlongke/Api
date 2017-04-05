5cba012c0924dc6c1f05ea8d3ada8ce273d235cb
clk528 <clk528@qq.com>
2017-04-05 16:13:04 +0800

    up

--- app/controllers/ExpressController.php
+++ app/controllers/ExpressController.php
@@ -10,10 +10,10 @@
 
 
 		public function pushAction(){
-			$kd = new Express();
+			$kd = new Express();			
+			$kdRs = $kd->poll("yuantong" ,"884525123079468982" ,"" ,"type=sb&id=1993528");
+			$this->log->info("推送结果|||".json_encode($kdRs));
 			var_dump($kd);
-			//$kdRs = $kd->poll("yuantong" ,"884525123079468982" ,"" ,"type=sb&id=1993528");
-			//$this->log->info("推送结果|||".json_encode($kdRs));
 		}
 
 		public function CallbackAction(){
